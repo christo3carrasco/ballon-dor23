20382ff74043cba97788486530f43c189f5a821a
Christopher Carrasco <christopher3carrasco@gmail.com>
2023-10-31 22:48:37 -0500

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { ReactRoutes } from "./routes/ReactRoutes";
+import { Header } from "./components/Header";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <ReactRoutes />
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Navbar, NavbarBrand } from "reactstrap";
+
+export const Header = () => {
+  return (
+    <Navbar color="dark" dark>
+      <NavbarBrand>Cipher Choice</NavbarBrand>
+    </Navbar>
+  );
+};
